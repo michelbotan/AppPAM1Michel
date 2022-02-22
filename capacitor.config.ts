83cb7044528c7bf92bd194e26bacefa3236d7346
Michel Eduardo Belaz Botan <canalcsgo01@gmail.com>
2022-02-22 09:05:44 -0300

    teste de projeto

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppPAM1Michel',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
